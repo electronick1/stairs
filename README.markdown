61d5304e824491c7a07cbfb28c6c1d48bac95eb1
oleg <oleg.ivye@gmail.com>
2019-01-02 02:58:06 +0300

    Update setup, update readme

---
+++ README.markdown
@@ -0,0 +1,13 @@
+
+
+Stairs. Framework for data processing.
+
+
+Stairs - It's a simple tool which allows you to juggle with data. 
+
+You can build data pipelines and make parallel/async/distributed calculations 
+for most of your data related tasks.
+
+Full documentation available here:
+https://electronick1.github.io/stairs_doc
+
--- setup.py
+++ setup.py
@@ -2,18 +2,19 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name="stairs",
+    name="stairs-project",
     version="0.0.0.1",
     author="Aleh Shydlouski",
     author_email="oleg.ivye@gmail.com",
     description="Framework for data processing",
-    keywords=['data', 'ai', 'distribute'],
+    keywords=['data', 'data-processing'],
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
         'tqdm',
         'ujson',
         'redis',
+        'stepist',
     ],
     classifiers=[],
     entry_points={'console_scripts': [
