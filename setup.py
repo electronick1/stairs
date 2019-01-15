0d65b2bd744dec38c49f34ed603b5a466f48c0ce
oleg <oleg.ivye@gmail.com>
2019-01-15 23:58:48 +0300

    inc version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="stairs-project",
-    version="0.0.0.2",
+    version="0.0.0.3",
     author="Aleh Shydlouski",
     author_email="oleg.ivye@gmail.com",
     description="Framework for data processing using data pipelines",
@@ -23,7 +23,7 @@ setup(
         'stepist'
     ],
     url='https://github.com/electronick1/stairs',
-    download_url='https://github.com/electronick1/stairs/archive/0.0.1.tar.gz',
+    download_url='https://github.com/electronick1/stairs/archive/0.0.3.tar.gz',
     classifiers=[],
     entry_points={'console_scripts': [
         'stairs-admin = stairs.services.management.commands:init_cli',
