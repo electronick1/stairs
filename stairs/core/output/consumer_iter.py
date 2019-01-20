73f55799e222395adc7bcfad0ed51f3445589253
oleg <oleg.ivye@gmail.com>
2019-01-21 00:25:49 +0300

    Fix consumer iter

--- stairs/core/output/consumer_iter.py
+++ stairs/core/output/consumer_iter.py
@@ -22,10 +22,20 @@ class ConsumerIter(Output):
             yield self.handler(**data)
 
     def jobs_iterator(self):
+        """
+        The way how it works a bit tricky.
+        We manually grab data from worker engine, but we can't just return
+        it because stepist session is not initialized.
+        For this reason we manually executing step and then just return
+        result of this step (result of run_job)
+        """
         project = project_session.get_project()
 
         while True:
-            yield project.stepist_app.worker_engine.receive_job(self.step)
+            job = project.stepist_app.worker_engine.receive_job(self.step)
+            data = self.step.receive_job(**job)
+            # it's last step in step and it will be dict with "run_job" key
+            yield data.get("run_job")
 
-    def run_job(self):
-        raise RuntimeError("Consumer iter can't process jobs as worker")
+    def run_job(self, **job_data):
+        return job_data
